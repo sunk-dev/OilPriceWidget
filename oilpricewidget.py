4e36292a9f1d0660e29b2fa43590c80d7fe74f53
seonkyoung <103514806+sunk-dev@users.noreply.github.com>
2026-05-04 20:50:54 +0900

    Update oilpricewidget.py

--- oilpricewidget.py
+++ oilpricewidget.py
@@ -21,24 +21,5 @@ result=crawling_oil_price()
 st.set_page_config(page_title="International Oil Price  Wiget", layout="centered")  
 st.title(f"국제 유가 위젯")
 st.html(crawling_oil_price())
-st.subheader("실시간 원유 가격 (Pandas Style)")
-st.dataframe(result, use_container_width=True)
 
-st.subheader("custom design")
-custom_css='''
-<style>
-    font-family: 'Noto Sans KR', Helvetica, AppleSDGothicNeo, sans-serif;
-    font-size: 14px;
-    border-collapse: collapse;
-    border-spacing: 0;
-    table-layout: fixed;
-    width: 100%;
-    margin-bottom: 8px;
-    border-top: solid 1px #666 !important;
-    border-bottom: solid 1px #b1b1b1;
-    color: #666;
-<style>
-'''
-table_result=result.to_html()
-table_html=st.markdown(custom_css+crawling_oil_price(),unsafe_allow_html=True)
  
