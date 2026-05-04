6feec70b991a093862a8bc8d5c1c567b75f0567f
seonkyoung <103514806+sunk-dev@users.noreply.github.com>
2026-05-04 20:44:13 +0900

    Update oilpricewidget.py

--- oilpricewidget.py
+++ oilpricewidget.py
@@ -2,23 +2,37 @@
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
-from premailer import transform
 from urllib.parse import urljoin # 상대 경로를 절대 경로로 바꿀 때 사용
 url='https://www.opinet.co.kr/glopcoilSelect.do#'
 response=requests.get(url)
-inlined_html = transform(response.text)
 
 def crawling_oil_price():
     if response.status_code==200:
         html=response.text
-        soup=BeautifulSoup(inlined_html,'html.parser')
+        soup=BeautifulSoup(html,'html.parser')
         title=soup.select_one('#glopcoilVO > div:nth-child(5) > table')
 
         return title
     else: 
         return response.status_code
-
+#crawling_oil_price()
 st.set_page_config(page_title="International Oil Price  Wiget", layout="centered")  
 st.title(f"국제 유가 위젯")
 st.html(crawling_oil_price())
+st.subheader("custom design")
+custom_css='''
+<style>
+    font-family: 'Noto Sans KR', Helvetica, AppleSDGothicNeo, sans-serif;
+    font-size: 14px;
+    border-collapse: collapse;
+    border-spacing: 0;
+    table-layout: fixed;
+    width: 100%;
+    margin-bottom: 8px;
+    border-top: solid 1px #666 !important;
+    border-bottom: solid 1px #b1b1b1;
+    color: #666;
+<style>
+'''
+table_html=st.markdown(custom_css+crawling_oil_price(),unsafe_allow_html=True)
  
